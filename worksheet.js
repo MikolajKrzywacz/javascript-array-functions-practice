2fb4c964e2e64c2d17a607310ce8ec614f539b92
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-06-29 16:42:29 +0200

    map() excercises done

--- worksheet.js
+++ worksheet.js
@@ -1,39 +1,47 @@
 const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: 172,
-        mass: 77,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: 202,
-        mass: 136,
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: 150,
-        mass: 49,
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: 188,
-        mass: 84,
-        eye_color: 'blue',
-        gender: 'male',
-    },
+  {
+    name: "Luke Skywalker",
+    height: 172,
+    mass: 77,
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: 202,
+    mass: 136,
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: 150,
+    mass: 49,
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: 188,
+    mass: 84,
+    eye_color: "blue",
+    gender: "male",
+  },
 ];
 
 //***MAP***
 //1. Get array of all names
+const names = characters.map((character) => character.name);
 //2. Get array of all heights
+const heights = characters.map((character) => character.height);
 //3. Get array of objects with just name and height properties
+const minifiedCharacters = characters.map((character) => {
+  return { name: character.name, height: character.height };
+});
 //4. Get array of all first names
+const firstName = characters.map((character) => {
+  character.name.split(" ")[0];
+});
 
 //***REDUCE***
 //1. Get total mass of all characters
